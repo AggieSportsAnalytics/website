97666e4c1cb710e983518605851e31bb0741fae2
yashdesh6 <yashdesh6@gmail.com>
2023-09-07 11:13:05 -0700

    sewyy

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "f533abf1648ce40d"; }
+/******/ 		__webpack_require__.h = function() { return "00996bcf32608f6b"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- app/events/page.tsx
+++ app/events/page.tsx
@@ -20,13 +20,13 @@ export default async function EventsPage() {
 					
 				</div>
 				<div className="w-full h-px bg-zinc-600" />
-				<h2 className="text-2xl font-bold tracking-tight text-zinc-100 sm:text-3xl">
+				<h2 className="text-xl font-semibold tracking-tight text-zinc-100 sm:text-2xl">
 						Fall Quarter 2023
 				</h2>
 				<div className="  text-zinc-200">
 					<div className="card  border mb-6 bg-transparent ">
 						<div className="card-body">
-							<h2 className="card-title text-zinc-100">First Meeting</h2>
+							<h2 className="card-title text-3xl font-extrabold mb-2 text-zinc-100">First Meeting</h2>
 							<p>New member or old, come attend to see what we were up to this summer and what we have planned this year!</p>
 							<div className="card-actions justify-end">
 							<button className="btn mt-2 bg-indigo-950 hover:bg-indigo-950 text-zinc-100 border border-zinc-300">Oct 18, 2023</button>
@@ -35,7 +35,7 @@ export default async function EventsPage() {
 					</div>
 					<div className="card  border mb-6 bg-zinc-950 bg-transparent">
 						<div className="card-body">
-							<h2 className="card-title text-zinc-100">General Recruitment</h2>
+							<h2 className="card-title text-3xl  font-extrabold mb-2 text-zinc-100">General Recruitment</h2>
 							<p>This date marks the week of recruitment! We will be recruiting project, business, and media team members.</p>
 							<div className="card-actions justify-end">
 							<button className="btn dro mt-2 bg-indigo-950 hover:bg-indigo-950 text-zinc-100 border border-zinc-300">Oct 24, 2023</button>
@@ -44,7 +44,7 @@ export default async function EventsPage() {
 					</div>
 					<div className="card  border  bg-zinc-950  bg-transparent ">
 						<div className="card-body">
-							<h2 className="card-title text-zinc-100">Sports Analytics Symposium</h2>
+							<h2 className="card-title text-3xl font-extrabold mb-2 text-zinc-100">Sports Analytics Symposium</h2>
 							<p>Join us at the ARC Ballroom as we host a panel of sports analysts from the 49ers, Kings, and more!</p>
 							<div className="card-actions justify-end">
 							<button className="btn mt-2 bg-indigo-950 hover:bg-indigo-950 text-zinc-100 border border-zinc-300">Nov 4, 2023</button>
