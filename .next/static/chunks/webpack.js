503bfd107b66f9818c5641cb2fa8c7d95992f302
yashdesh6 <yashdesh6@gmail.com>
2023-09-07 01:54:18 -0700

    colour

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "4e711ca510e93e29"; }
+/******/ 		__webpack_require__.h = function() { return "83520a4dad841a90"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- app/components/card.tsx
+++ app/components/card.tsx
@@ -23,7 +23,7 @@ export const Card: React.FC<PropsWithChildren> = ({ children }) => {
 	return (
 		<div
 			onMouseMove={onMouseMove}
-			className="overflow-hidden relative duration-700 border rounded-xl hover:bg-[#160F33]/25 group md:gap-8 hover:border-[#7477b2] border-zinc-600 "
+			className="overflow-hidden relative duration-700 border rounded-xl hover:bg-[#160F33]/25 group md:gap-8 hover:border-[#7477b2] border-zinc-300 "
 		>
 			<div className="pointer-events-none">
 				<div className="absolute inset-0 z-0  transition duration-1000 [mask-image:linear-gradient(black,transparent)]" />
